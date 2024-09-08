88d90cb415f882a50de9f5dd9e63ab5a447f88af
Mojtaba Tajik <onyfel@gmail.com>
2024-09-08 17:38:10 +0200

    Implement style save & load

--- Boop/Services/ThemeService.cs
+++ Boop/Services/ThemeService.cs
@@ -1,18 +1,40 @@
-﻿namespace Boop.Services;
+﻿using System.Text.Json;
+
+namespace Boop.Services;
 
 using System.Collections.Generic;
 
-public static class ThemeService
+public class ThemeService : IDisposable
 {
-    private static Dictionary<string, string> Styles { get; } = new()
-    {
-        { "OSVersion", "red bold" },
-        { "TotalMemory", "green italic" },
-        // Add other object names and their corresponding styles here
-    };
+    private static ThemeService? _instance;
+    public static ThemeService Instance => _instance ??= new ThemeService();
+    
+    private const string StylesPath = "styles.json";
+    private const string DefaultStyle = "white bold";
+    private readonly Dictionary<string, string>? _styles;
 
-    public static string GetStyle(string key)
+    private ThemeService()
     {
-        return Styles.TryGetValue(key, out var style) ? style : "white bold";
+        if (!File.Exists(StylesPath))
+        {
+            _styles = new();
+            SaveStyles();
+        }
+        
+        string stylesJson = File.ReadAllText(StylesPath);
+        _styles = JsonSerializer.Deserialize<Dictionary<string, string>>(stylesJson);
+    }
+
+    public string GetStyle(string key) => _styles?.GetValueOrDefault(key, DefaultStyle) ?? DefaultStyle;
+    
+    private void SaveStyles()
+    {
+        string stylesJson = JsonSerializer.Serialize(_styles);
+        File.WriteAllText(StylesPath, stylesJson);
+    }
+    
+    public void Dispose()
+    {
+        SaveStyles();
     }
 }
